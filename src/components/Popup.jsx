9ef6b14211b66109e03604b22c28840df7bc89e3
João Canabarro Campos <joaocampos963@icloud.com>
2024-12-05 11:48:47 +0100

    Add Confetti effect on pop-up page

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,10 +1,14 @@
 import "../css/Popup.css";
+import Confetti from 'react-confetti'
 
-const Popup = ({ message, onRestart }) => {
+const Popup = ({ message, turns, onRestart }) => {
   return (
-    <div className="popup-overlay">
+    <div className="popup-overlay" >
+      <Confetti
+        numberOfPieces="800" />
       <div className="popup">
         <h2>{message}</h2>
+        <h3> Number of turns: {turns}</h3>
         <button className="btn-restart" onClick={onRestart}>
           Restart Game
         </button>
